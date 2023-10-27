666a38fe075250b404cee64c0a1473df826c9ac8
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-10-26 18:31:59 -0600

    Update CPinitializer_list.h

--- src/CPinitializer_list.h
+++ src/CPinitializer_list.h
@@ -16,41 +16,42 @@
             using initializer_list = std::initializer_list<T>;
         #else
 
-            template<class T>
-            class initializer_list {
-            public:
-                using value_type = T;
-                using reference = const T&;
-                using const_reference = const T&;
-                using size_type = size_t;
-                using const_iterator = const T*;
+            #include <cstddef> // For size_t
 
-            private:
-                const_iterator _M_array;
-                size_type _M_len;
+            namespace cpstd {
 
+                template <typename T>
+                class initializer_list {
+                public:
+                    using value_type = T;
+                    using reference = const T&;
+                    using const_reference = const T&;
+                    using size_type = size_t;
+                    using const_iterator = const T*;
 
-            public:
-                constexpr initializer_list(const_iterator __a, size_type __l) noexcept
-                    : _M_array(__a), _M_len(__l) { }
+                private:
+                    const_iterator _M_array;
+                    size_type _M_len;
 
-                constexpr initializer_list() noexcept
-                    : _M_array(nullptr), _M_len(0) { }
+                public:
+                    constexpr initializer_list() noexcept : _M_array(nullptr), _M_len(0) {}
 
-                constexpr size_type size() const noexcept { return _M_len; }
-                constexpr const_iterator begin() const noexcept { return _M_array; }
-                constexpr const_iterator end() const noexcept { return _M_array + _M_len; }
+                    constexpr initializer_list(const T* array, size_type size) noexcept
+                        : _M_array(array), _M_len(size) {}
 
-                // Custom constructor to initialize with a brace-enclosed initializer list
-                constexpr initializer_list(std::initializer_list<T> il) noexcept
-                    : _M_array(il.begin()), _M_len(il.size()) { }
-            };
+                    constexpr size_type size() const noexcept { return _M_len; }
+                    constexpr const_iterator begin() const noexcept { return _M_array; }
+                    constexpr const_iterator end() const noexcept { return _M_array + _M_len; }
+                };
 
-            // Helper function to create initializer lists
-            template<class T>
-            constexpr initializer_list<T> make_initializer_list(const T* array, size_t size) {
-                return initializer_list<T>(array, size);
-            }
+                // Helper function to create initializer lists from arrays
+                template <typename T>
+                constexpr initializer_list<T> make_initializer_list(const T* array, size_t size) {
+                    return initializer_list<T>(array, size);
+                }
+
+            } // namespace cpstd
+
 
         #endif
     }
