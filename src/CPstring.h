69fe886a990c0c7a84aa7d7c3f6bfa25f5b69eab
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-10-28 14:12:51 -0600

    Update CPstring.h

--- src/CPstring.h
+++ src/CPstring.h
@@ -8,6 +8,7 @@
         #if defined(CPSTL_STRING_USING_STD_ALLOCATION) || defined(CPSTL_STRING_USING_CPP_ALLOCATION)
             #include <string>
         #else
+            #include <cstring.h> 
             #include <string.h> 
             #include <stdio.h>
         #endif
