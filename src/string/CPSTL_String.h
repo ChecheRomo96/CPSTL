dc378f283bb9cdbf60707166b04ed9ff936b77f7
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-10-31 17:15:04 -0600

    Update CPSTL_String.h

--- src/string/CPSTL_String.h
+++ src/string/CPSTL_String.h
@@ -14,6 +14,7 @@
     #elif defined(CPSTL_STRING_USING_C_ALLOCATION) 
         #include <string.h> 
         #include <stdio.h>
+        #include <stdlib.h>
     #endif
 
     #if defined(CPSTL_VECTOR_EXCEPTIONS_ENABLED) || defined(CPSTL_EXCEPTIONS_ENABLED)
