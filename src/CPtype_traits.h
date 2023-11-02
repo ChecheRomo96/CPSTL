5e46f23820ddce663a054ff1595c1e69568195e3
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-02 02:29:31 -0600

    Update CPtype_traits.h

--- src/CPtype_traits.h
+++ src/CPtype_traits.h
@@ -36,7 +36,7 @@
         template <typename T, typename U> inline constexpr bool is_same_v = cpstd::is_same<T, U>::value;
 
         template <typename T> struct is_same<T, T> { static constexpr bool value = true; };
-        template <typename T> inline constexpr bool is_same_v = cpstd::is_same<T, t>::value;
+        template <typename T> inline constexpr bool is_same_v = cpstd::is_same<T, T>::value;
 
         template <typename T> struct remove_const { using type = T; };
         template <typename T> struct remove_const<const T> { using type = T; };
