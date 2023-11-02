02d2fae6a6bc76a9af9accb4ffb09d4f9481fab2
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-02 02:01:08 -0600

    Update CPtype_traits.h

--- src/CPtype_traits.h
+++ src/CPtype_traits.h
@@ -40,14 +40,10 @@
         template <typename T> struct remove_const { using type = T; };
         template <typename T> struct remove_const<const T> { using type = T; };
 
-
         template<class T> struct remove_cv { typedef T type; };
         template<class T> struct remove_cv<const T> { typedef T type; };
         template<class T> struct remove_cv<volatile T> { typedef T type; };
         template<class T> struct remove_cv<const volatile T> { typedef T type; };
-
-        template<class T> struct remove_const { typedef T type; };
-        template<class T> struct remove_const<const T> { typedef T type; };
 
         template<class T> struct remove_volatile { typedef T type; };
         template<class T> struct remove_volatile<volatile T> { typedef T type; };
