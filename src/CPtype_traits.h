4b5e4964699b842dc3b16dac6a9ebef191855cc9
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-02 01:33:29 -0600

    Update CPtype_traits.h

--- src/CPtype_traits.h
+++ src/CPtype_traits.h
@@ -29,38 +29,16 @@
         template<class T>
         using remove_volatile = std::remove_volatile<T>;
     #else
-        template <typename T, typename U>
-        struct is_same<T,U>{
-            static constexpr bool value = false;
-        };
+        template <typename T, typename U> struct is_same<T,U>{ static constexpr bool value = false; };
+        template <typename T> struct is_same<T, T> { static constexpr bool value = true; };
+        template <typename T, typename U> inline constexpr bool is_same_v = cpstd::is_same<T, U>::value;
 
-        template <typename T>
-        struct is_same<T, T> {
-            static constexpr bool value = true;
-        };
+        template <typename T> struct remove_const { using type = T; };
+        template <typename T> struct remove_const<const T> { using type = T; };
 
-        template <typename T, typename U>
-        inline constexpr bool is_same_v = cpstd::is_same<T, U>::value;
-
-
-        template <typename T>
-        struct remove_const {
-            using type = T;
-        };
-
-        template <typename T>
-        struct remove_const<const T> {
-            using type = T;
-        };
-
-        template <class T>
-        struct remove_reference { using type = T; };
-
-        template <class T>
-        struct remove_reference<T&> { using type = T; };
-
-        template <class T>
-        struct remove_reference<T&&> { using type = T; };
+        template <class T> struct remove_reference { using type = T; };
+        template <class T> struct remove_reference<T&> { using type = T; };
+        template <class T> struct remove_reference<T&&> { using type = T; };
 
         template<class T> struct remove_cv { typedef T type; };
         template<class T> struct remove_cv<const T> { typedef T type; };
