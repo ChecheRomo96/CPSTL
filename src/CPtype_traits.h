713390512aa2bf13b4851ce75b112a008fb291d8
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-02 01:53:21 -0600

    Update CPtype_traits.h

--- src/CPtype_traits.h
+++ src/CPtype_traits.h
@@ -28,7 +28,7 @@
         template<class T>
         using remove_volatile = std::remove_volatile<T>;
     #else
-        template <typename T, typename U> struct is_same<T,U>{ static constexpr bool value = false; };
+        template <typename T, typename U> struct is_same{ static constexpr bool value = false; };
         template <typename T> struct is_same<T, T> { static constexpr bool value = true; };
         template <typename T, typename U> inline constexpr bool is_same_v = cpstd::is_same<T, U>::value;
 
