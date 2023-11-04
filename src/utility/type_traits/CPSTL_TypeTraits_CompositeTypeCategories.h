f1a790624063df3adb06914fd3b5a0849ba9cdd7
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-04 04:51:58 -0600

    Update CPSTL_TypeTraits_CompositeTypeCategories.h

--- src/utility/type_traits/CPSTL_TypeTraits_CompositeTypeCategories.h
+++ src/utility/type_traits/CPSTL_TypeTraits_CompositeTypeCategories.h
@@ -8,24 +8,12 @@
     #if defined(CPSTL_USING_STL)
 
         template <typename T> using is_arithmetic = std::is_arithmetic<T>;
-        template <typename T> inline constexpr bool is_arithmetic_v = cpstd::is_arithmetic<T>::value;
-        
         template <typename T> using is_compound = std::is_compound<T>;
-        
         template <typename T> using is_fundamental = std::is_fundamental<T>;
-        template <typename T> inline constexpr bool is_fundamental_v = cpstd::is_fundamental<T>::value;
-
         template <typename T> using is_member_pointer = std::is_member_pointer<T>;
-        template <typename T> inline constexpr bool is_member_pointer_v = cpstd::is_member_pointer<T>::value;
-
         template <typename T> using is_object = std::is_object<T>;
-        template <typename T> inline constexpr bool is_object_v = cpstd::is_object<T>::value;
-
         template <typename T> using is_reference = std::is_reference<T>;
-        template <typename T> inline constexpr bool is_reference_v = cpstd::is_reference<T>::value;
-
         template <typename T> using is_scalar = std::is_scalar<T>;
-        template <typename T> inline constexpr bool is_scalar_v = cpstd::is_scalar<T>::value;
     #else 
         template <typename T>
         struct is_reference : cpstd::integral_constant<bool,
@@ -52,6 +40,15 @@
         struct is_compound : cpstd::integral_constant <bool, !cpstd::is_fundamental<T>::value> {};
 
     #endif
+
+        
+        template <typename T> inline constexpr bool is_arithmetic_v = cpstd::is_arithmetic<T>::value;
+        template <typename T> inline constexpr bool is_fundamental_v = cpstd::is_fundamental<T>::value;
+        template <typename T> inline constexpr bool is_member_pointer_v = cpstd::is_member_pointer<T>::value;
+        template <typename T> inline constexpr bool is_object_v = cpstd::is_object<T>::value;
+        template <typename T> inline constexpr bool is_reference_v = cpstd::is_reference<T>::value;
+        template <typename T> inline constexpr bool is_scalar_v = cpstd::is_scalar<T>::value;
+
     }
 
 #endif//CPSTL_TYPE_TRAITS_COMPOSITE_TYPE_CATEGORIES_H
