e22ee4a663b2e156887ffb1091dbba7f25a0237d
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-03 19:18:02 -0600

    Update cpstd_type_traits.cpp

--- examples/AUnit_Tests/src/cpstd_type_traits.cpp
+++ examples/AUnit_Tests/src/cpstd_type_traits.cpp
@@ -327,9 +327,40 @@ TEST(CPSTL_TypeTraitsTest, IsEnum) {
 }
 
 TEST(CPSTL_TypeTraitsTest, IsFloatingPoint) {
-    ASSERT_TRUE((cpstd::is_floating_point<float>::value));
-    ASSERT_FALSE((cpstd::is_floating_point<int>::value));
-    ASSERT_TRUE((cpstd::is_floating_point<double>::value));
+    // Test case 1: Basic floating-point type identification
+    {
+        ASSERT_TRUE((cpstd::is_floating_point_v<float>));
+        ASSERT_TRUE((cpstd::is_floating_point_v<double>));
+    }
+
+    // Test case 2: Non-floating-point types
+    {
+        ASSERT_FALSE((cpstd::is_floating_point_v<int>));
+        ASSERT_FALSE((cpstd::is_floating_point_v<unsigned int>));
+        ASSERT_FALSE((cpstd::is_floating_point_v<long>));
+        ASSERT_FALSE((cpstd::is_floating_point_v<char>));
+    }
+
+    // Test case 3: Pointer types and references
+    {
+        float* ptr_float = nullptr;
+        double& ref_double = *ptr_float; // This should fail to compile, demonstrating it's not a floating-point reference
+
+        static_assert(!cpstd::is_floating_point_v<decltype(ptr_float)>, "Error: Pointer should not be floating-point");
+    }
+
+    // Test case 4: Negative scenarios - Non-conforming types
+    {
+        union MyUnion { int x; float y; };
+        ASSERT_FALSE((cpstd::is_floating_point_v<MyUnion>));
+    }
+
+    #if defined(CPSTL_USING_STL)
+    // Test case 5: Cross-verification - Compare with standard library traits
+    {
+        ASSERT_TRUE((cpstd::is_floating_point<double>::value == std::is_floating_point<double>::value));
+    }
+    #endif
 }
 
 TEST(CPSTL_TypeTraitsTest, IsFunction) {
