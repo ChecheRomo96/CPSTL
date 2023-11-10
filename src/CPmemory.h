dd77bd1a0660f062675b65f072f12bc37044326c
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-10 16:31:52 -0600

    Update CPmemory.h

--- src/CPmemory.h
+++ src/CPmemory.h
@@ -5,10 +5,10 @@
     #include <utility/CPSTL_allocator.h>
 
     namespace cpstd{
-
         template <typename InputIt, typename NoThrowForwardIt>
         NoThrowForwardIt uninitialized_move(InputIt first, InputIt last, NoThrowForwardIt d_first) {
             using ValueType = typename cpstd::iterator_traits<NoThrowForwardIt>::value_type;
+            using ConstPointer = typename cpstd::allocator_traits<typename cpstd::iterator_traits<NoThrowForwardIt>::allocator_type>::const_pointer;
 
             while (first != last) {
                 ::new (cpstd::addressof(*d_first)) ValueType(cpstd::move(*first));
@@ -22,7 +22,7 @@
         template <class ExecutionPolicy, class ForwardIt, class NoThrowForwardIt>
         NoThrowForwardIt uninitialized_move(ExecutionPolicy&& policy,
                                             ForwardIt first, ForwardIt last,
-                                            NoThrowForwardIt d_first) {
+                                            NoThrowForwardIt d_first) {  
             // Placeholder for parallel execution logic
             // In a real implementation, consider parallelizing the move operation based on the policy
             return uninitialized_move(first, last, d_first);
