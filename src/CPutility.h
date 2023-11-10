d226606aebb65938c53871023e16b4fc23f64ff2
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-10 11:10:37 -0600

    Update CPutility.h

--- src/CPutility.h
+++ src/CPutility.h
@@ -8,19 +8,19 @@
     #if defined CPSTL_USING_STL
     #else
         template <typename T>
-        T&& forward(typename std::remove_reference<T>::type& arg) noexcept {
+        T&& forward(typename cpstd::remove_reference<T>::type& arg) noexcept {
             return static_cast<T&&>(arg);
         }
 
         template <typename T>
-        T&& forward(typename std::remove_reference<T>::type&& arg) noexcept {
-            static_assert(!std::is_lvalue_reference<T>::value,
+        T&& forward(typename cpstd::remove_reference<T>::type&& arg) noexcept {
+            static_assert(!cpstd::is_lvalue_reference<T>::value,
                           "Cannot forward an rvalue as an lvalue.");
             return static_cast<T&&>(arg);
         }
     #endif
     }
-    
+
     #include <utility/CPSTL_Move.h>
     #include <utility/CPSTL_Iterator.h>
 
