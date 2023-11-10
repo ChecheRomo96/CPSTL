26f7ff480d63117a80502f190d0c29ccd46e3b08
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-10 15:35:16 -0600

    Update CPutility.h

--- src/CPutility.h
+++ src/CPutility.h
@@ -21,6 +21,16 @@
     #endif
     }
 
+    template <class T> 
+    void swap (T& a, T& b){
+      T c(std::move(a)); a=std::move(b); b=std::move(c);
+    }
+
+    template <class T, size_t N> 
+    void swap (T (&a)[N], T (&b)[N]){
+      for (size_t i = 0; i<N; ++i) swap (a[i],b[i]);
+    }
+
     #include <utility/CPSTL_Move.h>
     #include <utility/CPSTL_Iterator.h>
 
