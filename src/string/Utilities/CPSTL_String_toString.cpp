579e45b42917f94ff71aaf19af0b8bbd677e9a9a
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-11 01:13:28 -0600

    Update CPSTL_String_toString.cpp

--- src/string/Utilities/CPSTL_String_toString.cpp
+++ src/string/Utilities/CPSTL_String_toString.cpp
@@ -3,21 +3,135 @@
 cpstd::string cpstd::to_string(int val){
 #if defined(CPSTL_STRING_USING_STD_ALLOCATION) || defined(CPSTL_USING_STL)
     return std::to_string(val);
-#elif defined(CPSTL_STRING_USING_C_ALLOCATION)
+#else
+    string result;
+    bool isNegative = false;
+
+    // Handle the case where the value is negative
+    if (val < 0) {
+        isNegative = true;
+        val = -val;
+    }
+
+    // Special case for 0
+    if (val == 0) {
+        result.push_back('0');
+    }
+
+    // Build the string in reverse order
+    while (val > 0) {
+        char digit = '0' + static_cast<char>(val % 10);
+        result.push_back(digit);
+        val /= 10;
+    }
+
+    // Add the negative sign if necessary
+    if (isNegative) {
+        result.push_back('-');
+    }
+
+    // Reverse the string to get the correct order
+    size_t start = 0;
+    size_t end = result.size() - 1;
+    while (start < end) {
+        char temp = result[start];
+        result[start] = result[end];
+        result[end] = temp;
+        start++;
+        end--;
+    }
+
+    return result;
 #endif
 }
 
 cpstd::string cpstd::to_string(long val){
 #if defined(CPSTL_STRING_USING_STD_ALLOCATION) || defined(CPSTL_USING_STL)
     return std::to_string(val);
-#elif defined(CPSTL_STRING_USING_C_ALLOCATION)
+#else
+    string result;
+    bool isNegative = false;
+
+    // Handle the case where the value is negative
+    if (val < 0) {
+        isNegative = true;
+        val = -val;
+    }
+
+    // Special case for 0
+    if (val == 0) {
+        result.push_back('0');
+    }
+
+    // Build the string in reverse order
+    while (val > 0) {
+        char digit = '0' + static_cast<char>(val % 10);
+        result.push_back(digit);
+        val /= 10;
+    }
+
+    // Add the negative sign if necessary
+    if (isNegative) {
+        result.push_back('-');
+    }
+
+    // Reverse the string to get the correct order
+    size_t start = 0;
+    size_t end = result.size() - 1;
+    while (start < end) {
+        char temp = result[start];
+        result[start] = result[end];
+        result[end] = temp;
+        start++;
+        end--;
+    }
+
+    return result;
 #endif
 }
 
 cpstd::string cpstd::to_string(long long val){
 #if defined(CPSTL_STRING_USING_STD_ALLOCATION) || defined(CPSTL_USING_STL)
     return std::to_string(val);
-#elif defined(CPSTL_STRING_USING_C_ALLOCATION)
+#else
+    string result;
+    bool isNegative = false;
+
+    // Handle the case where the value is negative
+    if (val < 0) {
+        isNegative = true;
+        val = -val;
+    }
+
+    // Special case for 0
+    if (val == 0) {
+        result.push_back('0');
+    }
+
+    // Build the string in reverse order
+    while (val > 0) {
+        char digit = '0' + static_cast<char>(val % 10);
+        result.push_back(digit);
+        val /= 10;
+    }
+
+    // Add the negative sign if necessary
+    if (isNegative) {
+        result.push_back('-');
+    }
+
+    // Reverse the string to get the correct order
+    size_t start = 0;
+    size_t end = result.size() - 1;
+    while (start < end) {
+        char temp = result[start];
+        result[start] = result[end];
+        result[end] = temp;
+        start++;
+        end--;
+    }
+
+    return result;
 #endif
 }
 
