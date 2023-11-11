0580117c3e3586197624daae13e468ff42c9f1d3
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-10 23:07:40 -0600

    Update CPSTL_Vector.h

--- src/vector/CPSTL_Vector.h
+++ src/vector/CPSTL_Vector.h
@@ -671,9 +671,8 @@
                             //! @param n   New size for the container.
                             //! @param val Value to fill the container with. Each of the 'n' elements in the container will be initialized to a copy of this value.
                                 
-                                template <class InputIterator>  
+                                template <class InputIterator, cpstd::enable_if_t<!cpstd::is_pointer_v<T>>>  
                                 void assign (InputIterator first, InputIterator last){   
-                                    static_assert(cpstd::is_pointer_v<InputIterator>, "InputIt must be a pointer type");
                                     clear();
                                     resize(cpstd::distance(first, last));
                                     cpstd::copy(first, last, _Buffer);
