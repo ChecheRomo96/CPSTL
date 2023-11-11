df5454085b5c5628a7efd2228c21930514bbaef6
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-10 23:23:59 -0600

    Update CPmemory.h

--- src/CPmemory.h
+++ src/CPmemory.h
@@ -30,6 +30,17 @@
             // In a real implementation, consider parallelizing the move operation based on the policy
             return uninitialized_move(first, last, d_first);
         }
+
+
+        template<class InputIterator, class ForwardIterator>
+        ForwardIterator uninitialized_copy ( InputIterator first, InputIterator last,
+                                               ForwardIterator result )
+        {
+          for (; first!=last; ++result, ++first)
+            new (static_cast<void*>(&*result))
+              typename iterator_traits<ForwardIterator>::value_type(*first);
+          return result;
+        }
     }
 
 #endif//CPSTL_MEMORY_H
