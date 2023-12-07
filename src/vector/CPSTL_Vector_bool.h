3b536ea2c3a6fa8601cdd6ea84bfb238d0859135
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-07 16:58:17 -0600

    Update CPSTL_Vector_bool.h

--- src/vector/CPSTL_Vector_bool.h
+++ src/vector/CPSTL_Vector_bool.h
@@ -1082,24 +1082,24 @@
                             //! @tparam last The index of the last element to be erased.
 
                                 iterator erase(const_iterator first, const_iterator last) {
-                                    iterator beginIt = begin();  // Use non-const iterator
-                                    iterator endIt = end();      // Use non-const iterator
+                                    iterator beginIt = begin();
+                                    iterator endIt = end();
 
                                     if (first >= last) {
-                                        return end();  // Return iterator to the end as an indication of an error or no change
+                                        return end();
                                     }
 
-                                    auto range = cpstd::distance(first, last);
+                                    auto range = std::distance(first, last);
 
                                     // Move elements to fill the erased range
-                                    for (iterator it = first; it + range < endIt; ++it) {
-                                        *it = cpstd::move(*(it + range));
+                                    for (iterator it = std::next(beginIt, std::distance(cbegin(), first)); it + range < endIt; ++it) {
+                                        *it = std::move(*(it + range));
                                     }
 
                                     // Resize the container
                                     resize(size() - range);
 
-                                    return beginIt + static_cast<size_type>(cpstd::distance(beginIt, first));
+                                    return beginIt + static_cast<size_type>(std::distance(beginIt, first));
                                 }
                             //
                             //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
