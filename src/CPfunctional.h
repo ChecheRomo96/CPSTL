06fbeacf393102fa6841a16bbfbe18d45b0ab632
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-13 14:02:31 -0600

    Update CPfunctional.h

--- src/CPfunctional.h
+++ src/CPfunctional.h
@@ -13,13 +13,13 @@
                 using forward = std::forward<T>;
             #else
                 template <typename T>
-                constexpr T&& forward(typename std::remove_reference<T>::type& arg) noexcept {
+                constexpr T&& forward(typename cpstd::remove_reference<T>::type& arg) noexcept {
                     return static_cast<T&&>(arg);
                 }
 
                 template <typename T>
-                constexpr T&& forward(typename std::remove_reference<T>::type&& arg) noexcept {
-                    static_assert(!std::is_lvalue_reference<T>::value, "Can't forward an rvalue as an lvalue.");
+                constexpr T&& forward(typename cpstd::remove_reference<T>::type&& arg) noexcept {
+                    static_assert(!cpstd::is_lvalue_reference<T>::value, "Can't forward an rvalue as an lvalue.");
                     return static_cast<T&&>(arg);
                 }
             #endif
