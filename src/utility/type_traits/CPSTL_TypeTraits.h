f4cda1d29a017fcb38064f91a6114e7f7afa9ed4
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2024-02-07 17:30:21 -0600

    Update CPSTL_TypeTraits.h

--- src/utility/type_traits/CPSTL_TypeTraits.h
+++ src/utility/type_traits/CPSTL_TypeTraits.h
@@ -4,6 +4,7 @@
     #include "CPSTL_TypeTraits_HelperClasses.h"
     #include "CPSTL_TypeTraits_PrimaryTypeCategories.h"
     #include "CPSTL_TypeTraits_CompositeTypeCategories.h"
+    #include "CPSTL_TypeTraits_TypeProperties.h"
 
 
 #endif//CPSTL_TYPE_TRAITS_HELPER_H
